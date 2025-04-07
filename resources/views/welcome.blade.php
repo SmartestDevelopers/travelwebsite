9827f6a8987edcd2a8b72901abee154e5684f002
nosheeniftikhar <nosheeniftikhar00@gmail.com>
2025-04-07 02:59:53 -0700

    updated CTA on welcome.blade.php
    I addedd CTA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2932,5 +2932,65 @@
     <!-- ================================
     END INFO AREA
 ================================= -->
-
+    <!-- ================================
+    START CTA AREA
+================================= -->
+<section
+      class="cta-area subscriber-area section-bg-2 padding-top-60px padding-bottom-60px"
+    >
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-7">
+            <div class="section-heading">
+              <p class="sec__desc text-white-50 pb-1">Get Updates & More</p>
+              <h2 class="sec__title font-size-30 text-white">
+                Thoughtful thoughts to your inbox
+              </h2>
+            </div>
+            <!-- end section-heading -->
+          </div>
+          <!-- end col-lg-7 -->
+          <div class="col-lg-5">
+            <div class="subscriber-box">
+              <div class="contact-form-action">
+                <form action="#">
+                  <div class="input-box">
+                    <label class="label-text text-white"
+                      >Enter email address</label
+                    >
+                    <div class="form-group mb-0">
+                      <span class="la la-envelope form-icon"></span>
+                      <input
+                        class="form-control"
+                        type="email"
+                        name="email"
+                        placeholder="Email address"
+                      />
+                      <button
+                        class="theme-btn theme-btn-small submit-btn"
+                        type="submit"
+                      >
+                        Subscribe
+                      </button>
+                      <span class="font-size-14 pt-1 text-white-50"
+                        ><i class="la la-lock me-1"></i>Don't worry your
+                        information is safe with us.</span
+                      >
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <!-- end section-heading -->
+          </div>
+          <!-- end col-lg-5 -->
+        </div>
+        <!-- end row -->
+      </div>
+      <!-- end container -->
+    </section>
+    <!-- end cta-area -->
+    <!-- ================================
+    END CTA AREA
+================================= -->
 @endsection
